4825bbdb90eb94c30559772b897f0c464dcf0766
Sergio <smartinsantos@gmail.com>
2017-01-28 23:33:59 -0600

    added add movie form and component

--- _client/components/AddMovie.js
+++ _client/components/AddMovie.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-
 const Nav = React.createClass({
   getInitialState () {
     return {
@@ -13,11 +12,53 @@ const Nav = React.createClass({
   },
   render () {
     let renderedChild
-      if (!this.state.showForm) {
-        renderedChild = <button onClick={this.toggleMovieForm}>Add Movie</button>
-      } else {
-        renderedChild = <p>form</p>
-      }
+    if (!this.state.showForm) {
+      renderedChild = 
+        <div className='text-center'>
+          <button 
+            className='btn btn-primary'
+            onClick={this.toggleMovieForm}>
+            Add Movie
+          </button>
+        </div>
+    } else {
+      renderedChild = 
+        <div className='container'>
+          <div className="form-horizontal">
+            <div className="form-group">
+              <input type="text" className="form-control" id="title" placeholder="Title" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" id="year" placeholder="Year" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" id="genre" placeholder="Genre" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" id="rating" placeholder="Actors" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" id="rating" placeholder="Rating" />
+            </div>
+            
+            <div className="form-group">
+              <label htmlFor="inputFile">Poster</label>
+              <input type="file" id="inputFile" />
+              <p className="help-block">File Name</p>
+            </div>
+            
+            <div>
+              <button type="submit" className="btn btn-default">Save</button>
+              <button 
+                className='btn btn-info'
+                onClick={this.toggleMovieForm}>
+                Close
+              </button>
+            </div>
+          </div>
+          
+        </div>
+    }
     return (
       <div className='add-btn-form'>
         {renderedChild}
